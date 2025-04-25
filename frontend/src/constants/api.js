f6b94aa3146437855a5f7e8ea94fc18023109687
Shashikant Rupin <shashikantrupin123@gmail.com>
2025-04-25 11:54:22 +0530

    api updated

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,2 +1,2 @@
-export const api = `https://panini8-assignment-r49p.onrender.com/`;
+export const api = `https://panini8-assignment-r49p.onrender.com`;
 // export const api = `http://localhost:3000/api`;
